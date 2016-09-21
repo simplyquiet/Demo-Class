d74ae57f335f6495aef61028f3ba55df4d27a73a
simplyquiet <simplyquiet@users.noreply.github.com>
2016-09-21 15:13:28 -0400

    Filled in missing parts of header

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 //complete me
 
 #include <iostream>
+#include "MyClass.h"
 using namespace std;
 
 
 int main()
 {
+	MyClass Myvar;
+	MyClass My var2;
+	int var = 10;
+	string str;
 
+	cin>>var;
+	cout<<"var = "var<<endl;
+
+	cin>>Myvar;
+	cout<<Myvar<<endl;
+
+	cin>>str;
+	cout<<"str = "<<str;
 }
